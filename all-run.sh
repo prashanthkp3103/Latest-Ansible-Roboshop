a5225b61514282cadcc7ff9d15b9e4695eefadc8
prasanthbangs2016 <prasanthbangs@gmail.com>
2024-07-23 13:43:06 +0530

    ansible

--- all-run.sh
+++ all-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define an array of arguments
-arguments=("frontend" "mongo" "catalogue" "redis" "user" "cart" "mysql" "shipping")
+arguments=("frontend" "mongo" "catalogue" "redis" "user" "cart" "mysql" "shipping" "rabbitmq" "payment" "dispatch")
 
 # Loop through the arguments array
 for arg in "${arguments[@]}"
